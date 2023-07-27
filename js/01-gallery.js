286855d279669dc57bfbf495ba92a5f53ca8232d
Lashtabeg <tahcik@live.com>
2023-07-27 22:49:00 +0800

    Done!

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,39 +1,36 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 const ulGal = document.querySelector(`.gallery`);
-const markup = galleryItems.map(({preview,original,description})=> {
+const markup = galleryItems
+  .map(({ preview, original, description }) => {
     return `<li class="gallery__item">
     <a class="gallery__link" href="${original}">
       <img
         class="gallery__image"
         src="${preview}"
         data-source="${original}"
-        alt="${description}"/></a></li>`}).join(" ");
-      ulGal.insertAdjacentHTML("beforeend",markup);
+        alt="${description}"/></a></li>`;
+  })
+  .join(" ");
+ulGal.insertAdjacentHTML("beforeend", markup);
 
-ulGal.addEventListener(`click`,onImgClick);
+ulGal.addEventListener(`click`, onImgClick);
 
 function onImgClick(event) {
   event.preventDefault();
-    if(event.target.nodeName === !"IMG") {return};
-
-    const instance = basicLightbox.create(`
-    <img src="${event.target.dataset.source}" width="800" height="600">
-`)
-instance.show();
-};
-
-
-ulGal.addEventListener(`keydown`,onEscClose);
-
-function onEscClose(event) {
-  if(event.code === "ESCAPE"){
-     instance.close();
-     console.log (event)
+  if (event.target.nodeName === !"IMG") {
+    return;
   }
-    };
 
-
-
-
+  const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" width="800" height="600">
+`);
+  instance.show();
+  window.addEventListener(`keydown`, onEscClose);
+  function onEscClose(event) {
+    if (event.code !== "Escape") return;
+    instance.close();
+    window.removeEventListener(`keydown`, onEscClose);
+  }
+}
